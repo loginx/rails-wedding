4a6cbb68fb2f1fcf5b25ead949b807651c4889a7
Xavier Spriet <linkadmin@gmail.com>
2013-01-10 21:08:37 -0500

    Fix frozen string mailer bug

--- app/mailers/notifications.rb
+++ app/mailers/notifications.rb
@@ -1,12 +1,12 @@
 class Notifications < ActionMailer::Base
   layout 'notifications'
-  default from: ENV['DEVISE_EMAILS_FROM']
+  default from: ENV['DEVISE_EMAILS_FROM'].dup
 
   def dispatch_rsvp(rsvp)
     Rails.logger.warn("RSVP from mailer: #{rsvp.inspect}")
     @rsvp = Rsvp.find(rsvp['id'])
 
-    mail(to: ENV['NOTIFICATION_EMAIL'], subject: "[RSVP] New wedding RSVP submission")
+    mail(to: ENV['NOTIFICATION_EMAIL'].dup, subject: "[RSVP] New wedding RSVP submission")
   end
 
   def confirm_rsvp(guest)
